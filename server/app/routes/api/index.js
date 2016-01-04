1b85627a466ad6e04375949c381e4ee49e5b5fbf
Michael DeNatale <mikedeeno84@gmail.com>
2016-01-04 13:02:58 -0500

    updated route tree

--- server/app/routes/api/index.js
+++ server/app/routes/api/index.js
@@ -2,4 +2,5 @@ var router = require('express').Router();
 
 router.use('/hats', require('./hats'));
 router.use('/user', require('./user'));
+router.use('/reviews', require('./reviews'));
 module.exports = router;
