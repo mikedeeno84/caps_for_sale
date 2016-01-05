fa07b8377d7e2b81efc1b718be40467a1809b9a3
Michael DeNatale <mikedeeno84@gmail.com>
2016-01-05 15:45:17 -0500

    lets resolve conflicts again

--- browser/js/singlehat/singlehat.js
+++ browser/js/singlehat/singlehat.js
@@ -7,9 +7,28 @@ app.config(function($stateProvider){
 				return HatFactory.getOneHat($stateParams.hatId)
 			}
 		},
-		controller: function($scope, hat){
-
+		controller: function($scope, hat, $uibModal, $log){
 			$scope.hat = hat;
+			$scope.showModal = false;
+		    $scope.open = function () {
+
+		      var modalInstance = $uibModal.open({
+		        animation: $scope.animationsEnabled,
+		        templateUrl: '/js/common/directives/modal/modal.html',
+		        controller: 'ModalInstanceCtrl',
+		        resolve: {
+		          items: function () {
+		            return $scope.items;
+		          }
+		        }
+		      });
+
+		    modalInstance.result.then(function (selectedItem) {
+		      $scope.selected = selectedItem;
+		    }, function () {
+		      $log.info('Modal dismissed at: ' + new Date());
+		    });
+		  };
 		}
 	})
 })
