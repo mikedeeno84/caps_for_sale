e855e64b97b964a2fea9e8148f35e14a30bfdbd7
Michael DeNatale <mikedeeno84@gmail.com>
2016-01-05 12:22:17 -0500

    lets fix a conflict

--- server/db/models/review.js
+++ server/db/models/review.js
@@ -1,10 +1,20 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+let Hat = mongoose.model('Hat');
 var schema = new mongoose.Schema({
 	user: {type: Schema.Types.ObjectId, ref: 'User', required: true},
 	hat: {type: Schema.Types.ObjectId, ref: 'Hat', required: true},
 	rating: {type: Number, min:1, max:5, required: true},
 	body: {type: String}
 });
+
+schema.pre('save', function (next) {
+	var review = this
+	Hat.findOne({_id:review.hat})
+		.then(function(hat){
+			hat.reviews.push(review)
+			return hat.save()
+		}).then(next).then(null, console.log)
+})
 
 module.exports = mongoose.model("Review", schema);
